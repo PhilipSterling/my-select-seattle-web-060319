b7b95d450dd16a3b50f7c74cbeaef079adb94200
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 07:16:11 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  newarray = []
+  while i < collection.length
+      newarray[i] = yield(array[i])
+      i = i + 1
+    end
+    return newarray
 end
