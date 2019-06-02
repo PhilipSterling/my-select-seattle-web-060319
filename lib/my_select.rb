8cda937123c6472a791b9b73e283a2a46d300c13
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 07:19:14 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,8 +3,8 @@ def my_select(collection)
   j = 0
   newarray = []
   while i < collection.length
-    if(yield(array[i]))
-      newarray[j] = yield(array[i])
+    if(yield(collection[i]))
+      newarray[j] = yield(collecton[i])
       j = j + 1
     end
       i = i + 1
